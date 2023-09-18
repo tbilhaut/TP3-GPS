5da44c7ffcb77db878f0652ab67191a704e6f745
burguet <burguetlucas@gmail.com>
2023-09-18 17:33:25 +0200

    connexion1

--- bdd/bdd.php
+++ bdd/bdd.php
@@ -1,6 +1,6 @@
 <?php
     $ipserver="192.168.64.86";
-    $basename="nintendogs";
+    $basename="Base_PROJET";
     $login="root";
     $password="root";
     $GLOBALS["pdo"] = new PDO('mysql:host='.$ipserver.';dbname='.$basename.'',$login,$password);
--- index.php
+++ index.php
@@ -13,50 +13,76 @@ session_start();
   <title>Mon site - Connexion</title>
 </head>
 
+<?php
+try // Connexion à la BDD sur PHPMyAdmin
+    {
+      include ("bdd/bdd.php");
+
+    if(isset($_POST['connexion'])) {
+      $requete1 = "SELECT * FROM `User` WHERE `login`='".$_POST['login']."'AND `password`= SHA2('".$_POST['password']."', 256);";
+      $resultat=$pdo->query($requete1);
+
+        if($resultat->rowCount()>0) {
+          $utilisateur = $resultat->fetch();
+          $_SESSION['id_utilisateur'] = $utilisateur['id'];
+          header('location: accueil/accueil.php');
+        }
+    }
+
+    if(isset($_POST['inscription'])){ 
+      echo"salut";
+      if($_POST['logininscription'] != ''){
+    echo"salut";
+      if($_POST['confirmpassword'] == $_POST['passwordinscription']){
+      
+            $requete1 = "INSERT INTO `User`(`login`, `password`) VALUES ('".$_POST['login']."',SHA2('".$_POST['passwordinscription']."', 256))";
+            $resultat=$pdo->query($requete1);
+          }
+   
+        }
+       
+          }
+        
+}
+
+catch(Exception $error) {
+    $error->getMessage();
+}
+?>
 <body>
-  <div class="container">
-    <div class="logo">
-      <i class="fas fa-user"></i>
-    </div>
 
-    <div class="tab-body" data-id="connexion">
-      <form>
-        <div class="row">
-          <i class="far fa-user"></i>
-          <input type="email" class="input" placeholder="Adresse Mail">
-        </div>
-        <div class="row">
-          <i class="fas fa-lock"></i>
-          <input placeholder="Mot de Passe" type="password" class="input">
-        </div>
-        <a href="#" class="link">Mot de passe oublié ?</a>
-        <button class="btn" type="button">Connexion</button>
-      </form>
-    </div>
-
-    <div class="tab-body" data-id="inscription">
-      <form>
-        <div class="row">
-          <i class="far fa-user"></i>
-          <input type="email" class="input" placeholder="Adresse Mail">
-        </div>
-        <div class="row">
-          <i class="fas fa-lock"></i>
-          <input type="password" class="input" placeholder="Mot de Passe">
-        </div>
-        <div class="row">
-          <i class="fas fa-lock"></i>
-          <input type="password" class="input" placeholder="Confirmer Mot de Passe">
-        </div>
-        <button class="btn" type="button">Inscription</button>
-      </form>
-    </div>
-
-    <div class="tab-footer">
-      <a class="tab-link active" data-ref="connexion" href="javascript:void(0)">Connexion</a>
-      <a class="tab-link" data-ref="inscription" href="javascript:void(0)">Inscription</a>
-    </div>
+<div class="container">
+  <div id="connexion">
+    <h1 class="title">Hellø Wørld</h1>
+    <p class="paragraphe">
+      Veuillez entrer vos détails personnel et démarrez votre journée sur Hellø Wørld
+    </p>
+    <a href="#" type="submit" class="btn-link connexion">Se connecter</a>
   </div>
+  <div id="inscription">
+    <h1 class="title">Créer un compte</h1>
+    <p class="paragraphe">
+      Veuillez remplir tous les champs
+    </p>
+    <form class="formulaire">
+      <div class="group-form">
+        <input type="text" placeholder="Nom">
+        <div class="icon-user"></div>
+      </div>
+      <div class="group-form">
+        <input type="email" placeholder="mail">
+        <div class="icon-mail"></div>
+      </div>
+      <div class="group-form">
+        <input type="password" placeholder="password">
+        <div class="icon-password"></div>
+      </div>
+      <div class="group-form">
+        <input type="submit" class="inscription" value="S'inscrire">
+      </div>
+    </form>
+  </div>
+</div>
 </body>
 
 </html>
