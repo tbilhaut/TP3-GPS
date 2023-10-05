772621870fb2efaa39ce8c1a7acc4824a3aa05d1
eloiselcr <eloise.lecronier@gmail.com>
2023-10-05 16:50:09 +0200

    -> commit du jeudi soir

--- admin.php
+++ admin.php
@@ -58,7 +58,7 @@ if (isset($_POST['deconnexion'])) {
     <title>GPS  - Espace Admin</title>
 
     <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -67,7 +67,7 @@ if (isset($_POST['deconnexion'])) {
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Custom styles for this page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
